4a9001b53d7464d9c1b5040835d3802b3999e1ab
AymanBAHHOU <44382293+AymanBAHHOU@users.noreply.github.com>
2018-11-28 11:40:51 +0000

    commit

--- LabGit/src/ma/emi/g4/Hello.java
+++ LabGit/src/ma/emi/g4/Hello.java
@@ -3,8 +3,7 @@ package ma.emi.g4;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
+		System.out.println("Hello World");	
 		System.out.println("Hello world 2");
 	}
 
