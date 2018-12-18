9fc25c8fcfc4e6a5246d63289425d09e49e305ec
AymanBAHHOU <44382293+AymanBAHHOU@users.noreply.github.com>
2018-12-18 15:38:25 +0000

    Commit

--- LabGit/src/ma/emi/g4/Hello.java
+++ LabGit/src/ma/emi/g4/Hello.java
@@ -3,12 +3,6 @@ package ma.emi.g4;
 public class Hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World");	
-=======
-		System.out.println("Hello World");
-		System.out.println("Hello world 2");
->>>>>>> branch 'master' of https://github.com/AymanBAHHOU/LabGit.git
 		System.out.println("Hello world 2");
 	}
 
